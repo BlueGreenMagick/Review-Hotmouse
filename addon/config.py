fdd4ebd9220e27cc98dd12f33a9d7c604684e5c7
bluegreenmagick <bluegreenmagick@gmail.com>
2021-08-16 15:40:56 +0900

    create class HotkeyTabManager and move functions there

--- addon/config.py
+++ addon/config.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional, List
+from typing import Callable, Optional, List, Union, Literal
 
 from aqt.qt import *
 
@@ -45,19 +45,63 @@ def create_dropdown(
     return dropdown
 
 
-def hotkey_tabs(conf_window: ConfigWindow) -> None:
+class HotkeyTabManager:
     button_opts = [b.name for b in Button]
     action_opts = list(ACTIONS.keys())
     mode_opts = ["press", "click", "wheel"]
     wheel_opts = ["up", "down"]
 
-    def on_mode_change(layout: ConfigLayout, optidx: int, ddidx: int) -> None:
+    def __init__(
+        self, tab: ConfigLayout, side: Union[Literal["q"], Literal["a"]]
+    ) -> None:
+        self.tab = tab
+        self.config_window = tab.config_window
+        self.side = side
+        self.setup_tab()
+
+    def setup_tab(self) -> None:
+        tab = self.tab
+        self.rows_layout = self.tab.vlayout()
+        btn_layout = tab.hlayout()
+        add_btn = QPushButton("+  Add New ")
+        add_btn.clicked.connect(
+            lambda _: self.add_row(f"{self.side}_click_right", "<none>")
+        )
+        add_btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        add_btn.setCursor(QCursor(Qt.PointingHandCursor))
+        btn_layout.addWidget(add_btn)
+        btn_layout.stretch()
+        btn_layout.setContentsMargins(0, 20, 0, 5)
+        tab.setSpacing(0)
+        tab.addLayout(btn_layout)
+        tab.stretch()
+
+    def create_layout(self) -> ConfigLayout:
+        return ConfigLayout(self.config_window, QBoxLayout.LeftToRight)
+
+    def clear_rows(self) -> None:
+        """Clear all rows in tab. Run before setup_rows."""
+        for i in range(self.rows_layout.count()):
+            widget = self.rows_layout.itemAt(0).widget()
+            self.rows_layout.removeWidget(widget)
+            widget.deleteLater()
+
+    def setup_rows(self) -> None:
+        hotkeys = conf.get("hotkeys")
+        for hotkey in hotkeys:
+            if hotkey[0] == self.side:
+                self.add_row(hotkey, hotkeys[hotkey])
+
+    def on_mode_change(self, layout: ConfigLayout, optidx: int, ddidx: int) -> None:
         """Handler for when mode dropdown changes"""
+        button_opts = self.button_opts
+        mode_opts = self.mode_opts
+        wheel_opts = self.wheel_opts
         mode = mode_opts[optidx]
         if mode == "press":
             layout.removeWidget(layout.dropdowns.pop(ddidx + 1))
             create_dropdown(layout, button_opts[0], button_opts)
-            create_dropdown(layout, "click", mode_opts, on_mode_change)
+            create_dropdown(layout, "click", mode_opts, self.on_mode_change)
             create_dropdown(layout, button_opts[0], button_opts)
         else:
             while len(layout.dropdowns) > ddidx + 1:
@@ -68,16 +112,19 @@ def hotkey_tabs(conf_window: ConfigWindow) -> None:
             else:  # mode == "wheel"
                 create_dropdown(layout, wheel_opts[0], wheel_opts)
 
-    def hotkey_layout(hotkey: str) -> Optional[ConfigLayout]:
-        """ hotkey eg: `press_left_click_right`. Without `q_` or `a_`. """
-        layout = ConfigLayout(conf_window, QBoxLayout.LeftToRight)
+    def hotkey_layout(self, hotkey: str) -> Optional[ConfigLayout]:
+        """ hotkey eg: `q_press_left_click_right`. Returns None if hotkey is invalid. """
+        button_opts = self.button_opts
+        mode_opts = self.mode_opts
+        wheel_opts = self.wheel_opts
 
-        hotkeylist = hotkey.split("_")
-        while len(hotkeylist):
-            if len(hotkeylist) == 1:
-                return None
-            mode = hotkeylist.pop(0)
-            btn = hotkeylist.pop(0)
+        layout = self.create_layout()
+        hotkeylist = hotkey[2:].split("_")
+        if len(hotkeylist) % 2 != 0:
+            return None
+        for i in range(0, len(hotkeylist), 2):
+            mode = hotkeylist[i]
+            btn = hotkeylist[i + 1]
             if mode not in mode_opts:
                 return None
             if mode == "wheel" and btn not in wheel_opts:
@@ -85,29 +132,32 @@ def hotkey_tabs(conf_window: ConfigWindow) -> None:
             if mode in ("press", "click") and btn not in button_opts:
                 return None
 
-            create_dropdown(layout, mode, mode_opts, on_mode_change)
+            create_dropdown(layout, mode, mode_opts, self.on_mode_change)
             if mode == "wheel":
                 create_dropdown(layout, btn, wheel_opts)
             else:
                 create_dropdown(layout, btn, button_opts)
         return layout
 
-    def action_layout(action: str) -> Optional[ConfigLayout]:
-        layout = ConfigLayout(conf_window, QBoxLayout.LeftToRight)
+    def action_layout(self, action: str) -> Optional[ConfigLayout]:
+        """Returns None if action string is invalid."""
+        if action not in self.action_opts:
+            return None
+        layout = self.create_layout()
         layout.stretch()
         layout.text("&nbsp;<b>→</b>&nbsp;", html=True)
-        create_dropdown(layout, action, action_opts)
+        create_dropdown(layout, action, self.action_opts)
         return layout
 
-    def build_row(rows_layout: ConfigLayout, hotkey: str, action: str) -> None:
-        hlay = hotkey_layout(hotkey)
-        alay = action_layout(action)
+    def add_row(self, hotkey: str, action: str) -> None:
+        hlay = self.hotkey_layout(hotkey)
+        alay = self.action_layout(action)
         if hlay and alay:
             container = QWidget()
-            layout = ConfigLayout(conf_window, QBoxLayout.LeftToRight)
+            layout = self.create_layout()
             layout.setContentsMargins(0, 4, 2, 4)  # decrease margin
             container.setLayout(layout)
-            rows_layout.addWidget(container)
+            self.rows_layout.addWidget(container)
             layout.addLayout(hlay)
             layout.addLayout(alay)
             label = QLabel("&nbsp;<a href='/' style='text-decoration:none;'>❌</a>")
@@ -115,49 +165,26 @@ def hotkey_tabs(conf_window: ConfigWindow) -> None:
             layout.addWidget(label)
 
             def remove(l: str) -> None:
-                rows_layout.removeWidget(container)
+                self.rows_layout.removeWidget(container)
                 container.deleteLater()
 
             label.linkActivated.connect(remove)
 
-    def add_tab() -> None:
-        if conf_window.main_tab.count() > 1:
-            conf_window.main_tab.widget(2).deleteLater()
-            conf_window.main_tab.widget(1).deleteLater()
-            conf_window.main_tab.removeTab(2)
-            conf_window.main_tab.removeTab(1)
+    def on_update(self) -> None:
+        self.clear_rows()
+        self.setup_rows()
 
-        q_tab = conf_window.add_tab("Question Side Hotkeys")
-        a_tab = conf_window.add_tab("Answer Side Hotkeys")
-        q_rows_layout = q_tab.vlayout()
-        a_rows_layout = a_tab.vlayout()
-        hotkeys = conf.get("hotkeys")
-        for hotkey in hotkeys:
-            if hotkey[0] == "q":
-                build_row(q_rows_layout, hotkey[2:], hotkeys[hotkey])
-            elif hotkey[0] == "a":
-                build_row(a_rows_layout, hotkey[2:], hotkeys[hotkey])
-            else:
-                continue
-        for tab in (q_tab, a_tab):
-            btn_layout = tab.hlayout()
-            add_btn = QPushButton("+  Add New ")
-            add_btn.clicked.connect(
-                lambda _, rows=tab.itemAt(0): build_row(rows, "click_right", "<none>")
-            )
-            add_btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-            add_btn.setCursor(QCursor(Qt.PointingHandCursor))
-            btn_layout.addWidget(add_btn)
-            btn_layout.stretch()
-            btn_layout.setContentsMargins(0, 20, 0, 5)
-            tab.setSpacing(0)
-            tab.addLayout(btn_layout)
-            tab.stretch()
 
-    conf_window.widget_updates.append(add_tab)
+def hotkey_tabs(conf_window: ConfigWindow) -> None:
+    q_tab = conf_window.add_tab("Question Hotkeys")
+    q_manager = HotkeyTabManager(q_tab, "q")
+    a_tab = conf_window.add_tab("AnswerHotkeys")
+    a_manager = HotkeyTabManager(a_tab, "a")
+    conf_window.widget_updates.append(q_manager.on_update)
+    conf_window.widget_updates.append(a_manager.on_update)
 
 
 conf = ConfigManager()
 conf.use_custom_window()
 conf.add_config_tab(general_tab)
-conf.on_window_open(hotkey_tabs)
+conf.add_config_tab(hotkey_tabs)
